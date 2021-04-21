fa5944383bbe0a01474e80e84e5bc470f1cd0071
Seth Musker <sethmusker@gmail.com>
2021-04-21 17:35:43 +0200

    Update vcf2conStruct.R

--- vcf2conStruct.R
+++ vcf2conStruct.R
@@ -1,21 +1,4 @@
 
-require(vcfR)
-require(adegenet)
-require(fields)
-require(dplyr)
-require(argparser)
-
-p <- arg_parser("convert VCF to conStruct. Outputs <outname>.RData file. \n
-                To load into R for conStruct analysis use load('<outname>.RData').\n
-                The object will be named conStruct_object \n\n
-                Resuires the following packages: vcfR, adegenet, fields, dplyr, argparser.\n
-                Run using Rscript, e.g.\n
-                Rscript vcf2conStruct.R --vcf my_file.vcf.gz --metadatafile my_meta.txt --outname my_conStruct",hide.opts = T)
-
-# Add a positional argument
-p <- add_argument(p, "--vcf", help="vcf file")
-p <- add_argument(p, "--metadatafile", help="file with *no header* with columns in this order: 'sample population longitude latitude'")
-p <- add_argument(p, "--outname", help="<name of output>.RData")
 
 # functions
 vcf2gi<-function(vcf,metadatafile){
@@ -52,12 +35,32 @@ vcf2conStruct<-function(vcf,metadatafile,outname){
   save(conStruct_object,file=paste(outname,".RData",sep=""))
 }
 
+require(argparser)
+
+p <- arg_parser("convert VCF to conStruct. Outputs <outname>.RData file. \n
+                To load into R for conStruct analysis use load('<outname>.RData').\n
+                The object will be named conStruct_object \n\n
+                Resuires the following packages: vcfR, adegenet, fields, dplyr, argparser.\n
+                Run using Rscript, e.g.\n
+                Rscript vcf2conStruct.R --vcf my_file.vcf.gz --metadatafile my_meta.txt --outname my_conStruct",hide.opts = T)
+
+# Add a positional argument
+p <- add_argument(p, "--vcf", help="vcf file (can be [b]gzipped)")
+p <- add_argument(p, "--metadatafile", help="file with *no header* with columns in this order: 'sample population longitude latitude'")
+p <- add_argument(p, "--outname", help="<name of output>.RData")
+
+
 # parse
 args<-parse_args(p)
-if(length(args)<3) {
+
+if(anyNA(c(args$vcf,args$metadatafile,args$outname))) {
   # Print the help message
   print(p)
 }else{
+  require(vcfR)
+  require(adegenet)
+  require(fields)
+  require(dplyr)
   vcf2conStruct(args$vcf,args$metadatafile,args$outname)
 }
 
